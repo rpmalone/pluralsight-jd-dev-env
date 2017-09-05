97ed87c0c47ca4a613baa10a88d2f7daa1e209d9
Ryan Malone <rymalone@Ryans-MacBook-Pro.local>
2017-09-05 11:19:32 -0700

    update webpack to support a production build export

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,4 +1,13 @@
 export default function getBaseUrl() {
-  const inDevelopment = window.location.hostname === 'localhost';
-  return inDevelopment ? 'http://localhost:3001/' : '/';
+  return getQueryStringPrarameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+}
+
+function getQueryStringPrarameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
